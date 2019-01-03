5e45777bd5deb538a83104ffb7ae994ea4ecb4b4
Saikat Bhadury <info2programmer@gmail.com>
2019-01-03 15:03:40 +0530

    Home page impliment

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Text, View, Image, Dimensions, ScrollView, StyleSheet, StatusBar, Platform, FlatList } from 'react-native';
+import { Text, View, Image, Dimensions, ScrollView, StyleSheet, StatusBar, Platform } from 'react-native';
 import TextTicker from 'react-native-text-ticker';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Swiper from 'react-native-swiper';
 import NavigationBar from 'react-native-navbar';
-import { SearchBar, ListItem, List  } from 'react-native-elements';
+import { SearchBar, Divider, Card, Button, Rating  } from 'react-native-elements';
 
 const MyStatusBar = ({ backgroundColor, ...props }) => (
   <View style={[styles.statusBar, { backgroundColor }]}>
@@ -17,85 +17,111 @@ const titleConfig = {
   title: 'KHAABARWALA'
 };
 
-
-const list = [
-  {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  },
-];
-
-
-
-
-
 const width = Dimensions.get('window').width;
 export default class App extends React.Component {
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <MyStatusBar backgroundColor="#d7d832" barStyle="dark-content" />
+      <View style={{ flex: 1 }}>
+        <MyStatusBar backgroundColor="#CCCC00" barStyle="dark-content" />
         <NavigationBar
           style={{ backgroundColor: 'yellow' }}
           title={titleConfig}
         />
         <SearchBar
           placeholder='Search By Restaurent' />
-        <View style={styles.contentView}>
-          <View>
-            <View style={styles.container}>
-              <Swiper style={styles.wrapper} height={200} horizontal={true} autoplay>
-                <View style={styles.slide} title={<Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>}>
-                  <Image resizeMode='stretch' style={styles.image} source={require('./images/banner1.jpg')} />
-                </View>
-                <View style={styles.slide} title={<Text numberOfLines={1}>Why Stone split from Garfield</Text>}>
-                  <Image resizeMode='stretch' style={styles.image} source={require('./images/banner3.jpg')} />
-                </View>
-                <View style={styles.slide} title={<Text numberOfLines={1}>Big lie behind Nine’s new show</Text>}>
-                  <Image resizeMode='stretch' style={styles.image} source={require('./images/banner2.jpg')} />
-                </View>
-              </Swiper>
+        <ScrollView style={styles.container}>
+          <View style={styles.contentView}>
+            <View>
+              <View style={styles.container}>
+                <Swiper style={styles.wrapper} height={200} horizontal={true} autoplay>
+                  <View style={styles.slide} title={<Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>}>
+                    <Image resizeMode='stretch' style={styles.image} source={require('./images/banner1.jpg')} />
+                  </View>
+                  <View style={styles.slide} title={<Text numberOfLines={1}>Why Stone split from Garfield</Text>}>
+                    <Image resizeMode='stretch' style={styles.image} source={require('./images/banner3.jpg')} />
+                  </View>
+                  <View style={styles.slide} title={<Text numberOfLines={1}>Big lie behind Nine’s new show</Text>}>
+                    <Image resizeMode='stretch' style={styles.image} source={require('./images/banner2.jpg')} />
+                  </View>
+                </Swiper>
+              </View>
             </View>
+            <View style={styles.headerContainer}>
+              <TextTicker
+                style={{ fontSize: 24, height: 48, marginTop: 2, padding: 5, }}
+                duration={16000}
+                marqueeOnStart
+                loop
+                scroll
+                marqueeDelay={1000}
+                marqueeResetDelay={1000}> <Icon color="white" name="star" size={20} style={{ color: 'red' }} /> Todays' Delivary Free With Khaabarwala App <Icon color="white" name="star" size={20} style={{ color: 'red' }} /></TextTicker>
+            </View>
+            <Divider style={{ backgroundColor: '#D3D3D3', marginTop: 2 }} />
+            <Text style={{ color: '#8a8a5c', fontSize: 20, margin: 8 }}>List of restaurant</Text>
+            <Card
+              title='WOW MOMO'
+              image={require('./images/banner1.jpg')}>
+              <Rating
+                showRating
+                type="star"
+                fractions={1}
+                startingValue={3.6}
+                imageSize={20}
+                style={{ paddingVertical: 5 }}
+              />
+              <Text style={{ marginBottom: 10 }}>
+                14 B, Anil Roy Rd, Hemanta Mukherjee Sarani, lake Terrace, Ballygunge, Kolkata, West Bengal 700029</Text>
+              <Button
+                icon={<Icon name='code' color='#ffffff' />}
+                backgroundColor='#03A9F4'
+                buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+                title='VIEW NOW' />
+            </Card>
+            <Card
+              title='PIZZA NATION'
+              image={require('./images/banner2.jpg')}>
+              <Rating
+                showRating
+                type="star"
+                fractions={1}
+                startingValue={4.6}
+                imageSize={20}
+                style={{ paddingVertical: 5 }}
+              />
+              <Text style={{ marginBottom: 10 }}>14 B, Anil Roy Rd, Hemanta Mukherjee Sarani, lake Terrace, Ballygunge, Kolkata, West Bengal 700029</Text>
+              <Button
+                icon={<Icon name='code' color='#ffffff' />}
+                backgroundColor='#03A9F4'
+                buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+                title='VIEW NOW' />
+            </Card>
+            <Card
+              title='Ebala Obala'
+              image={require('./images/banner3.jpg')}>
+              <Rating
+                showRating
+                type="star"
+                fractions={1}
+                startingValue={4.2}
+                imageSize={20}
+                style={{ paddingVertical: 5 }}
+              />
+              <Text style={{ marginBottom: 10 }}>14 B, Anil Roy Rd, Hemanta Mukherjee Sarani, lake Terrace, Ballygunge, Kolkata, West Bengal 700029</Text>
+              <Button
+                icon={<Icon name='code' color='#ffffff' />}
+                backgroundColor='#03A9F4'
+                buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+                title='VIEW NOW' />
+            </Card>
+            <Divider style={{ backgroundColor: '#D3D3D3', marginTop: 5 }} />
           </View>
-          <View style={styles.headerContainer}>
-            <TextTicker
-              style={{ fontSize: 24 }}
-              duration={16000}
-              marqueeOnStart
-              loop
-              scroll
-              marqueeDelay={1000}
-              marqueeResetDelay={1000}> <Icon color="white" name="star" size={20} style={{ color: 'red' }} /> In publishing and graphic design, lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content. <Icon color="white" name="star" size={20} style={{ color: 'red' }} /></TextTicker>            
-          </View>
-          <Text h3>Heading 4</Text>
-          <List>
-            <FlatList
-              data={list}
-              renderItem={this.renderRow}
-              keyExtractor={item => item.name}
-            />
-          </List>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </View>
+
     );
 
   }
-  renderRow({ item }) {
-    return (
-      <ListItem
-        roundAvatar
-        title={item.name}
-        subtitle={item.subtitle}
-        avatar={{ uri: item.avatar_url }}
-      />
-    )
-  }
+
 }
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
 const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
